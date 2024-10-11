03e35dcb5f0c6c588610d8042dc8f83c3fdb6577
X1 Nano <X1 Nano@LAPTOP-B0V7KGGN>
2024-10-11 21:09:20 +0700

    Add project files.

---
+++ ThanhBinh_Lap04FE/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ThanhBinh_Lap04FE
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ThanhBinh_Lap04FE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThanhBinh_Lap04FE.MvcApplication" Language="C#" %>
